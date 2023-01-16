d132136623d8a5e7d93ccd87d0e0e81f065e749d
Tevissaur <berserkboar@protonmail.com>
2023-01-15 19:32:28 -0500

    pivot back to mono repo

--- client/src/interfaces/IUser.ts
+++
@@ -1,6 +0,0 @@
-import { World } from "./IWorld";
-
-export interface User {
-    username: string;
-    worlds: Array<World>
-}
--- server/config/connection.ts
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/forenedb");
-
-export default mongoose.connection;
